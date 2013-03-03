f56d00fa9bddfc84f61dbe5237eb143f408c022c
Ram Krishnan <kriyative@gmail.com>
2013-03-02 22:25:16 -0800

    Use clojuredocs REST api first, then fallback to a google search.

--- clojuredocs.el
+++ clojuredocs.el
@@ -3,6 +3,7 @@
 
 (require 'clojure-mode-ext)
 (require 'clojure-slime-ext)
+(require 'json)
 
 (defconst clojuredocs-uri-prefix "http://clojuredocs.org")
 (defconst google-uri-prefix "http://google.com")
@@ -34,17 +35,81 @@
 (defun javadocs-uri-formatter (sym format-string)
   (format format-string (replace-regexp-in-string "\\." "/" sym)))
 
+(defun clojuredocs-process-response-json (status
+                                          on-success
+                                          success-args
+                                          on-error
+                                          error-args)
+  (goto-char (point-min))
+  (search-forward-regexp "^HTTP/1.[01] \\([0-9][0-9][0-9]\\)" nil t)
+  (unwind-protect
+      (if (equal "200" (match-string 1))
+          (condition-case c
+              (progn
+                (message "url-retrieve: OK")
+                (search-forward-regexp "^$" nil t)
+                (let ((json (json-read)))
+                  (apply on-success json success-args)))
+            (error
+             (message "json-read error: %S" c)
+             (apply on-error :json-error error-args)))
+        (funcall on-error :url-error))
+    (url-mark-buffer-as-dead (current-buffer))))
+
+(defun assoc-val (key alist)
+  (let ((a (assoc key alist)))
+    (when a
+      (cdr a))))
+
+(defun clojuredocs-response-success (json)
+  (let ((tmpfilename (make-temp-file "clojuredocs" nil ".html"))
+        (title (format "clojuredocs search: %d results" (length json))))
+    (message "tmpfilename: %s" tmpfilename)
+    (with-temp-file tmpfilename
+      (insert
+       (format "<html>\n<head><title>%s</title></head>\n" title))
+      (insert "<body>\n"
+              (format "<h1>%s</h1>\n" title)
+              "<ul>\n")
+      (loop for el across json
+            do (let ((url (assoc-val 'url el))
+                     (ns (assoc-val 'ns el))
+                     (name (assoc-val 'name el)))
+                 (insert
+                  (format "<li><a href='%s#top'>%s/%s</a></li>\n"
+                          url
+                          ns
+                          name))))
+      (insert "</ul></body></html>\n"))
+    (browse-url
+     (browse-url-file-url tmpfilename))))
+
+(defun clojuredocs-response-error (error)
+  (message "Problem communicating with clojuredocs (%s)" error))
+
+(defun clojuredocs-find-or-search (sap)
+  (url-retrieve (format "http://api.clojuredocs.org/search/%s" sap)
+                'clojuredocs-process-response-json
+                (list (lambda (json sap)
+                        (if json
+                            (clojuredocs-response-success json)
+                          (browse-url
+                           (format "%s/search?q=%s+clojure"
+                                   google-uri-prefix
+                                   sap))))
+                      (list sap)
+                      'clojuredocs-response-error
+                      nil)))
+
 (defun clojuredocs-render (sap type)
   (destructuring-bind (sap type)
       values
     (cond
      ((eq :var type)
-      (browse-url
-       (or (clojuredocs-find-uri sap)
-           (format "%s/search?q=%s"
-                   clojuredocs-uri-prefix
-                   (first (last (split-string sap "/")))
-                   (replace-regexp-in-string "[ ]+" "+" sap)))))
+      (let ((sap-uri (clojuredocs-find-uri sap)))
+        (if sap-uri
+            (browse-url sap-uri)
+          (clojuredocs-find-or-search sap))))
      ((eq :class type)
       (browse-url
        (or (clojuredocs-find-uri sap javadocs-prefix-map 'javadocs-uri-formatter)
